409549105ab63d4df4ef47de7a5bd4549922cf95
fernandasolc <fernandaoliv.8272@gmail.com>
2024-03-07 15:32:23 -0300

    um

---
+++ testeAfinidade/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import testeafinidade from '../img/'
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Tururu</h2>
+      <p>Começar:</p>
+      <button>Iniciar</button>
+      <img>src="" alt=""</img>
+    </div>
+  )
+}
+
+export default Welcome
