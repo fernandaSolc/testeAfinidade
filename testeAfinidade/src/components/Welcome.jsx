ea4a03529e490f4efb55f4750d997decb1fffb08
fernandasolc <fernandaoliv.8272@gmail.com>
2024-03-08 15:28:57 -0300

    button

--- testeAfinidade/src/components/Welcome.jsx
+++ testeAfinidade/src/components/Welcome.jsx
@@ -1,14 +1,17 @@
-import testeafinidade from '../img/'
+import "./Welcome.css";
+import testeafinidade from "../img/quiz.svg";
 
 const Welcome = () => {
   return (
-    <div>
-      <h2>Tururu</h2>
-      <p>Começar:</p>
-      <button>Iniciar</button>
-      <img>src="" alt=""</img>
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>
+        Iniciar
+      </button>
+      <img src={testeafinidade} alt="Início do Quiz" />
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
